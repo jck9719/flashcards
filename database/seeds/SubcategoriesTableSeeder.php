c6436a99f9114d1bac810cee61387e77e84fe4b7
Juliusz <gaunt1111@gmail.com>
2018-05-08 23:00:35 +0200

    next fixes

--- database/seeds/SubcategoriesTableSeeder.php
+++ database/seeds/SubcategoriesTableSeeder.php
@@ -14,22 +14,22 @@ class SubcategoriesTableSeeder extends Seeder
         DB::table('subcategories')->insert([[
             'id' => 1,
             'name' => 'Dom',
-            'description' => 'Kategoria zawierające słówka z działu dom.',
+            'description' => 'Subkategoria zawierające słówka z działu dom.',
             'category_id' => DB::table('categories')->where('name', 'Ogólne')->pluck('id')->first()
         ],[
             'id' => 2,
             'name' => 'Rodzina',
-            'description' => 'Kategoria zawierająca słówka z działu praca',
+            'description' => 'Subategoria zawierająca słówka z działu rodzina',
             'category_id' => DB::table('categories')->where('name', 'Ogólne')->pluck('id')->first()
         ],[
             'id' => 3,
             'name' => 'Informatyka',
-            'description' => 'Lorem ipsum dolor sit amet',
+            'description' => 'Subategoria zawierająca słówka z działu informatyka',
             'category_id' => DB::table('categories')->where('name', 'Specjalistyczne')->pluck('id')->first()
         ],[
             'id' => 4,
             'name' => 'Medycyna',
-            'description' => 'Lorem ipsum dolor sit amet',
+            'description' => 'Subategoria zawierająca słówka z działu medycyna',
             'category_id' => DB::table('categories')->where('name', 'Specjalistyczne')->pluck('id')->first()
         ]
         ]);
