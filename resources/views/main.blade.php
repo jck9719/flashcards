2a2c11a1312b69a9e397f2a9fed1efd3385f507d
Juliusz <gaunt1111@gmail.com>
2018-05-09 13:55:46 +0200

    1.3

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,6 +5,8 @@
   <p>Zarejestruj się by móc oglądać swoje wyniki. Miłej nauki :)</p>
 </div>
 <div class="container">
+	<div class="col-md-4 text-center"> 
 	<a href="/start" class="btn btn-info" role="button">Start!</a>
+</div>
 </div>
 @endsection
