8b1e951041fcc66528feecaca240afc2a49ab190
Juliusz <gaunt1111@gmail.com>
2018-05-09 20:09:46 +0200

    1.3

--- resources/views/start/welcome.blade.php
+++ resources/views/start/welcome.blade.php
@@ -18,44 +18,11 @@
                                         <div class="col-md-3">
                                             {{ $category['description'] }}
                                         </div>
-                                        @if(Auth::check())
-                                            @if(Auth::user()->role_id == 1)
-                                                <div class="col-md-2 text-center pd">
-                                                    <a class="btn btn-primary pull-right" aria-label="Left Align" href="/category/{{ $category['id'] }}/update">
-                                                        <span class="btn-text"><i class="fas fa-pencil-alt"></i>  Edytuj</span>
-                                                    </a>
-                                                </div>
-                                                <div class="col-md-2 text-center pd">
-                                                    <form action="/category/{{ $category['id'] }}"
-                                                          method="post">
-                                                        {{ method_field('DELETE') }}
-                                                        {{ csrf_field() }}
-                                                        <button type="submit" class="btn btn-danger pull-right" aria-label="Left Align">
-                                                            <span class="btn-text">
-                                                                <i class="fas fa-trash-alt"></i>  Usuń
-                                                            </span>
-                                                        </button>
-                                                    </form>
-                                                </div>
-                                            @endif
-                                        @endif
                                     </div>
                                 </li>
                             @endforeach
                         </ul>
                         <div class="divider" style="margin-top: 20px"></div>
-                        @if(Auth::check() && Auth::user()->role_id == 1)
-                            <div class="row">
-                                <div class="col-md-3"></div>
-                                <div class="col-md-6 text-center">
-                                    <a href="/category/new/create" class="btn btn-warning"
-                                       aria-label="Left Align">
-                                        <span class="btn-text"><i
-                                                    class="fas fa-plus-circle"></i>Dodaj nową kategorię</span>
-                                    </a>
-                                </div>
-                            </div>
-                        @endif
                     </div>
                 </div>
             </div>
