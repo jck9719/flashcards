23efa2bd891eba687f1c95dc73decbae68ae4c22
Juliusz <gaunt1111@gmail.com>
2018-05-09 11:04:08 +0200

    0.3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,18 +10,7 @@ class HomeController extends Controller
 
     public function __construct(Request $request)
     {
-    if(! function_exists('clearSession')) {
-    function clearSession(\Illuminate\Http\Request $request) {
-        $request->session()->pull('words');
-        $request->session()->pull('language1');
-        $request->session()->pull('language2');
-        $request->session()->pull('isFirstLanguage');
-        $request->session()->pull('wordsCount');
-        $request->session()->pull('pointedWordsCount');
-        $request->session()->pull('wrongWords');
         clearSession($request);
-    }
-    }
     }
 
 
