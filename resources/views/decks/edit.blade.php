8d19be4c1ba7afee1bb53c04b83314fdfe2afea0
Juliusz <gaunt1111@gmail.com>
2018-05-09 19:41:35 +0200

    1.3

--- resources/views/decks/edit.blade.php
+++ resources/views/decks/edit.blade.php
@@ -70,7 +70,7 @@
                                     </div>
                                 @endforeach
                             </div>
-                            @if(($isSubcategoryEditor) || (Auth::user()->role_id == 1))
+                            @if($isSubcategoryEditor)
                                 <div class="form-group">
                                     <div class="row pd">
                                         <div class="col-md-3"></div>
