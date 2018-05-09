e247fd526238e6be1cab510c81d393d6094649af
Juliusz <gaunt1111@gmail.com>
2018-05-09 16:43:45 +0200

    1.3

--- resources/views/dbusers.blade.php
+++ resources/views/dbusers.blade.php
@@ -33,13 +33,13 @@
                                             </div>
                                             <div class="col-md-2 text-center pd">
                                                 <a class="btn btn-primary pull-right" aria-label="Left Align"
-                                                   href="/admin/user/{{ $user['id'] }}">
+                                                   href="/dbusers/user/{{ $user['id'] }}">
                                                     <span class="btn-text"><i
                                                                 class="fas fa-pencil-alt"></i> Edytuj</span>
                                                 </a>
                                             </div>
                                             <div class="col-md-2 text-center pd">
-                                                <form action="/admin/user/{{ $user['id'] }}"
+                                                <form action="/dbusers/user/{{ $user['id'] }}"
                                                       method="post">
                                                     {{ method_field('DELETE') }}
                                                     {{ csrf_field() }}
