fd22037e08e35497b08b260ef70fdbea182285d3
Juliusz <gaunt1111@gmail.com>
2018-05-09 14:53:55 +0200

    1.3

--- resources/views/subcategories/subcategory.blade.php
+++ resources/views/subcategories/subcategory.blade.php
@@ -67,7 +67,7 @@
                             <div class="row">
                                 <div class="col-md-3"></div>
                                 <div class="col-md-6 text-center">
-                                    <a href="#" class="btn btn-warning"
+                                    <a href="/category/{{ $category['id'] }}/subcategory/{{ $subcategory['id] }}/decks/create" class="btn btn-warning"
                                        aria-label="Left Align">
                                         <span class="btn-text"><i
                                                     class="fas fa-plus-circle"></i>Dodaj nowy zestaw</span>
