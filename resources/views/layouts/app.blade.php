b399a8adf370e4ad55dd9ac8f5f103b67c7ca8e4
Juliusz <gaunt1111@gmail.com>
2018-05-09 10:23:13 +0200

    changing title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,8 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Flashcards') }}
+                   // {{ config('app.name', 'Flashcards') }}
+                   
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
