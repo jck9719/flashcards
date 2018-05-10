b7f23d778f756d3847bde4dd245cb1a8b4c7b502
Juliusz <gaunt1111@gmail.com>
2018-05-10 10:26:48 +0200

    1.5

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,6 +17,11 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-       
+        if (Auth::guard($guard)->check()) {
+            return $next($request);
+            return redirect('/');
+        }
+
+       return redirect('/');
     }
 }
